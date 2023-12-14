ded373b1239124c489ee18c417ef09dc3502307f
Alon Valadji <alon@ronin.co.il>
2023-12-14 21:14:34 +0200

    :recycle: cards same height

--- apps/dashboard/app/posts/page.tsx
+++ apps/dashboard/app/posts/page.tsx
@@ -18,14 +18,14 @@ export default async function Page() {
     <div className="flex flex-col gap-4">
       <Heading size="xl">Posts</Heading>
 
-      <ul className="grid gap-4 grid-cols-3">
+      <ul className="grid gap-4 grid-cols-3 place-items-stretch">
         {posts.map((post: { title: string; body: string; id: string }) => (
-          <li key={post.id}>
-            <Card>
+          <li key={post.id} className="flex">
+            <Card className="flex flex-col">
               <CardHeader>
                 <CardTitle>{post.title}</CardTitle>
               </CardHeader>
-              <CardContent>{post.body}</CardContent>
+              <CardContent className="flex-1">{post.body}</CardContent>
               <CardFooter className="flex justify-between">
                 <Link href={`posts/${post.id}`}>
                   <Button>Read More</Button>
